6703680068bc2d7bb3105f63f6687357119ea0d4
Gara Luzardo <garaluzardo@gmail.com>
2025-02-16 23:07:21 +0000

    Iteration 3

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function AddBeerPage() {
   // State variables to store the values of the form inputs. You can leave these as they are.
   const [name, setName] = useState("");
   const [tagline, setTagline] = useState("");
   const [description, setDescription] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  const [image_url, setImageUrl] = useState("");
   const [firstBrewed, setFirstBrewed] = useState("");
   const [brewersTips, setBrewersTips] = useState("");
   const [attenuationLevel, setAttenuationLevel] = useState(0);
@@ -21,20 +23,42 @@ function AddBeerPage() {
   const handleAttenuationLevel = (e) => setAttenuationLevel(e.target.value);
   const handleContributedBy = (e) => setContributedBy(e.target.value);
 
-
+  const navigate = useNavigate();
 
   // TASK:
   // 1. Create a function to handle the form submission and send the form data to the Beers API to create a new beer.
   // 2. Use axios to make a POST request to the Beers API.
   // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    const newBeer = {
+      name: name,
+      tagline: tagline,
+      description: description,
+      image_url: image_url,
+      first_brewed: firstBrewed,
+      brewers_tips: brewersTips,
+      attenuation_level: attenuationLevel,
+      contributed_by: contributedBy,
+    };
 
+    axios
+      .post("https://ih-beers-api2.herokuapp.com/beers/new", newBeer)
+      .then((response) => {
+        console.log("New beer created:", response.data);
+        navigate("/beers");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   // Structure and the content of the page showing the form for adding a new beer. You can leave this as it is.
   return (
     <>
       <div className="d-inline-flex flex-column w-100 p-4">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Name</label>
           <input
             className="form-control mb-4"
@@ -69,9 +93,9 @@ function AddBeerPage() {
           <input
             className="form-control mb-4"
             type="text"
-            name="imageUrl"
+            name="image_url"
             placeholder="Image URL"
-            value={imageUrl}
+            value={image_url}
             onChange={handleImageUrl}
           />
 
@@ -122,7 +146,7 @@ function AddBeerPage() {
             value={contributedBy}
             onChange={handleContributedBy}
           />
-          <button className="btn btn-primary btn-round">Add Beer</button>
+          <button type="submit" className="btn btn-primary btn-round">Add Beer</button>
         </form>
       </div>
     </>
